a3a468bfe66b4a5c6bd8ff415abbc369473fbf28
Dinara83 <108099459+Dinara83@users.noreply.github.com>
2022-11-24 20:23:17 +0200

    start, color-switcher - 01

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,26 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+let timerBtnId = null;
+
+refs.startBtn.addEventListener('click', onClickStartBtn);
+refs.stopBtn.addEventListener('click', onClickStopBtn);
+
+function onClickStartBtn() {
+  timerBtnId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.setAttribute('disabled', 'true');
+}
+function onClickStopBtn() {
+  clearInterval(timerBtnId);
+  refs.startBtn.removeAttribute('disabled', 'true');
+  refs.stopBtn.setAttribute('disabled', 'true');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
