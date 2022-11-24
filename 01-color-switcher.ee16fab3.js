cb8463e58db076fc6c33b5fe3ec78b4f705cf74e
Dinara83 <108099459+Dinara83@users.noreply.github.com>
2022-11-24 20:23:09 +0000

    Deploying to gh-pages from @ Dinara83/goit-js-hw-09@40bcce4e75164cb4385a6a2f97f5321db4b72071 🚀

--- 01-color-switcher.ee16fab3.js
+++
@@ -1,2 +0,0 @@
-const t={body:document.querySelector("body"),startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};let e=null;t.startBtn.addEventListener("click",(function(){e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBtn.setAttribute("disabled","true")})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.startBtn.removeAttribute("disabled","true"),t.stopBtn.setAttribute("disabled","true")}));
-//# sourceMappingURL=01-color-switcher.ee16fab3.js.map
